2f92bbc209c0732488192e4abfd8c4455a427bc1
Hong Baojin <13809735212.163.com>
2020-06-19 18:19:52 +0800

    add silde-nav

--- js/index.js
+++ js/index.js
@@ -1,30 +1,52 @@
-$(function(){
-  // 鼠标点击切换
-  function clickTab(aimEle, cont, classname){
-    $(aimEle).click(function(){
-      // 阻止点击穿透
-      event.stopPropagation();
-      var that = this;
-      $(aimEle).removeClass(classname);
-      $(that).addClass(classname);
+$(function () {
+  theaMsForm($('.sign-in'));
+  
+  // 头部显示app下载二维码
+  $('.s-wap').hover(function () {
+    $('.app-download').show();
+  }, function () {
+    $('.app-download').hide();
+  });
 
-      $(cont).click(function(){
+  // 点击元素不是同辈, 点击空白处内容隐藏
+  function noPeerClick(btn, cont, classname) {
+    $(btn).each(function () {
+      var that = this;
+      $(that).click(function () {
+        $(btn).removeClass(classname);
+        $(that).addClass(classname);
         // 阻止点击穿透
         event.stopPropagation();
       });
-      
       // 点击空白处隐藏
-      $(document).click(function(){
+      $(document).click(function () {
         $(that).removeClass(classname);
       });
     });
+    // 显示的内容里有点击事件
+    $(cont).click(function () {
+      event.stopPropagation();
+    });
   };
   // 免费兑换
-  clickTab('.dh', '.dh-detail', 'on');
+  noPeerClick('.dh', '.dh-detail', 'on');
   // 更多班次
-  clickTab('.r-more', null, 'on');
+  noPeerClick('.r-more', null, 'on');
+
+  // 点击元素是同辈
+  function peerClick(btn, cont, classname) {
+    $(btn).each(function (i) {
+      var that = this;
+      $(that).click(function () {
+        $(that).addClass(classname).siblings().removeClass(classname);
+        $($(cont)[i]).addClass(classname).siblings().removeClass(classname);
+      });
+    });
+  }
+  // 套餐切换
+  peerClick('.l-head span', '.l-detail .d-file', 'on');
   // 科目
-  clickTab('.r-subject li', null, 'on');
+  peerClick('.r-subject li', null, 'on');
 
   // 文件树
   function fileTree(outerBox, headBox, classname) {
@@ -41,5 +63,4 @@ $(function(){
   fileTree('.f-second', '.s-head', 'show');
   // 第3层
   fileTree('.s-three', '.t-head', 'show');
-
 });
